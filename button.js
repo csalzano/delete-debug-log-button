1decb7758164e970fbdaeca5872c9c2ad48231a5
Corey Salzano <corey.salzano@gmail.com>
2022-11-01 11:17:47 -0400

    Bug fix. Stops deleting debug.log when the Viewer link is clicked.

--- button.js
+++ button.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-	jQuery('#wp-admin-bar-delete_debug_log a').on('click',function(){
+	jQuery('#wp-admin-bar-delete_debug_log > a').on('click',function(){
 		jQuery('.spinner.spinner-delete-debug-log').addClass('is-active').css({ "height": ".8em", "width": ".8em", "background-size": ".8em", "margin-left": ".3em", "float": "none" });
 		var data = {
 			action: 'delete_debug_log',
